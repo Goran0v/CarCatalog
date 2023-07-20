7def529d9b314b90f25eb92302c948df10b2a509
Lyudmil Goranov <l.goranov07@gmail.com>
2023-07-20 20:45:14 +0300

    Created the Car class

--- CarCatalog.Common/EntityValidationConstants.cs
+++ CarCatalog.Common/EntityValidationConstants.cs
@@ -2,6 +2,16 @@
 {
     public class EntityValidationConstants
     {
+        public static class CarValidations
+        {
+            public const int BrandMinValue = 3;
+            public const int BrandMaxValue = 20;
 
+            public const int ModelMinValue = 3;
+            public const int ModelMaxValue = 40;
+
+            public const int CarTypeMinValue = 3;
+            public const int CarTypeMaxValue = 20;
+        }
     }
 }
---
+++ CarCatalog.Data.Models/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CarCatalog.Common.EntityValidationConstants.CarValidations;
+
+namespace CarCatalog.Data.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(BrandMaxValue)]
+        public string Brand { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ModelMaxValue)]
+        public string Model { get; set; } = null!;
+
+        [Required]
+        [MaxLength(CarTypeMaxValue)]
+        public string CarType { get; set; } = null!;
+
+        public int HorsePower { get; set; }
+
+        public float EngineDisplacement { get; set; }
+
+        public float Mileage { get; set; }
+
+        public float FuelConsumption { get; set; }
+    }
+}
