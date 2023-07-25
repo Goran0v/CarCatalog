ea30b32f54f7ace3556aeb06f57ee08336f833a1
Lyudmil Goranov <l.goranov07@gmail.com>
2023-07-25 16:59:34 +0300

    Shortened the Car class by implementing a Car Info class

--- CarCatalog.Data.Models/Car.cs
+++ CarCatalog.Data.Models/Car.cs
@@ -8,41 +8,9 @@ namespace CarCatalog.Data.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        [MaxLength(BrandMaxValue)]
-        public string Brand { get; set; } = null!;
+        public Guid CarInfoId { get; set; }
 
-        [Required]
-        [MaxLength(ModelMaxValue)]
-        public string Model { get; set; } = null!;
-
-        [Required]
-        [MaxLength(CarTypeMaxValue)]
-        public string CarType { get; set; } = null!;
-
-        public int HorsePower { get; set; }
-
-        public float EngineDisplacement { get; set; }
-
-        public int Mileage { get; set; }
-
-        public int Weight { get; set; }
-
-        public float FuelConsumption { get; set; }
-
-        public int PriceForSale { get; set; }
-
-        public TransmissionType Transmission { get; set; }
-
-        public EngineType Engine { get; set; }
-
-        [Required]
-        [MaxLength(ImageUrlMaxValue)]
-        public string ImageUrl { get; set; } = null!;
-
-        [Required]
-        [MaxLength(DescriptionMaxValue)]
-        public string Description { get; set; } = null!;
+        public virtual CarInfo CarInfo { get; set; } = null!;
 
         public Guid? BuyerId { get; set; }
 
---
+++ CarCatalog.Data.Models/CarInfo.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static CarCatalog.Common.EntityValidationConstants.CarValidations;
+
+namespace CarCatalog.Data.Models
+{
+    public class CarInfo
+    {
+        public CarInfo()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(BrandMaxValue)]
+        public string Brand { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ModelMaxValue)]
+        public string Model { get; set; } = null!;
+
+        [Required]
+        [MaxLength(CarTypeMaxValue)]
+        public string CarType { get; set; } = null!;
+
+        public int HorsePower { get; set; }
+
+        public float EngineDisplacement { get; set; }
+
+        public int Mileage { get; set; }
+
+        public int Weight { get; set; }
+
+        public float FuelConsumption { get; set; }
+
+        public int PriceForSale { get; set; }
+
+        public TransmissionType Transmission { get; set; }
+
+        public EngineType Engine { get; set; }
+
+        [Required]
+        [MaxLength(ImageUrlMaxValue)]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxValue)]
+        public string Description { get; set; } = null!;
+    }
+}
