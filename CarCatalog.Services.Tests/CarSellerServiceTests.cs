7b9efcda14a8421315373032a8920188fbf361d2
Lyudmil Goranov <l.goranov07@gmail.com>
2023-08-12 16:49:46 +0300

    Tested the Car Seller service

--- CarCatalog.Services.Tests/CarSellerServiceTests.cs
+++ CarCatalog.Services.Tests/CarSellerServiceTests.cs
@@ -45,5 +45,105 @@ namespace CarCatalog.Services.Tests
 
             Assert.IsFalse(result);
         }
+
+        [Test]
+        public async Task CarSellerExistsByPhoneNumberAsyncShouldReturnTrueWhenExists()
+        {
+            string phoneNumber = Seller.PhoneNumber;
+
+            bool result = await this.carSellerService.CarSellerExistsByPhoneNumberAsync(phoneNumber);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task CarSellerExistsByPhoneNumberAsyncShouldReturnFalseWhenDoesNotExists()
+        {
+            string phoneNumber = Buyer.PhoneNumber;
+
+            bool result = await this.carSellerService.CarSellerExistsByPhoneNumberAsync(phoneNumber);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task CarSellerExistsByUsernameAsyncShouldReturnTrueWhenExists()
+        {
+            string username = Seller.Username;
+
+            bool result = await this.carSellerService.CarSellerExistsByUsernameAsync(username);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task CarSellerExistsByUsernameAsyncShouldReturnFalseWhenDoesNotExists()
+        {
+            string username = Buyer.Username;
+
+            bool result = await this.carSellerService.CarSellerExistsByUsernameAsync(username);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task GetCarSellerUsernameByUserIdAsyncShouldReturnUsernameWhenTrue()
+        {
+            string username = Seller.Username;
+
+            string result = await this.carSellerService.GetCarSellerUsernameByUserIdAsync(CarSellerUser.Id.ToString());
+
+            Assert.That(username, Is.EqualTo(result));
+        }
+
+        [Test]
+        public async Task GetCarSellerUsernameByUserIdAsyncShouldNotReturnUsernameWhenFalse()
+        {
+            string username = Buyer.Username;
+
+            string result = await this.carSellerService.GetCarSellerUsernameByUserIdAsync(CarSellerUser.Id.ToString());
+
+            Assert.That(username, Is.Not.EqualTo(result));
+        }
+
+        [Test]
+        public async Task GetSellerIdByUserIdAsyncShouldReturnIdWhenTrue()
+        {
+            string id = Seller.Id.ToString();
+
+            string? result = await this.carSellerService.GetSellerIdByUserIdAsync(CarSellerUser.Id.ToString());
+
+            Assert.That(id, Is.EqualTo(result));
+        }
+
+        [Test]
+        public async Task GetSellerIdByUserIdAsyncShouldNotReturnIdWhenFalse()
+        {
+            string id = Buyer.Id.ToString();
+
+            string? result = await this.carSellerService.GetSellerIdByUserIdAsync(CarSellerUser.Id.ToString());
+
+            Assert.That(id, Is.Not.EqualTo(result));
+        }
+
+        [Test]
+        public async Task HasACarWithIdAsyncShouldReturnTrueWhenHas()
+        {
+            string id = CarSellerUser.Id.ToString();
+
+            bool result = await this.carSellerService.HasACarWithIdAsync(id, Car.Id.ToString());
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task HasACarWithIdAsyncShouldReturnFalseWhenHasNot()
+        {
+            string id = CarBuyerUser.Id.ToString();
+
+            bool result = await this.carSellerService.HasACarWithIdAsync(id, Car.Id.ToString());
+
+            Assert.IsFalse(result);
+        }
     }
 }
--- CarCatalog.Services.Tests/DatabaseSeeder.cs
+++ CarCatalog.Services.Tests/DatabaseSeeder.cs
@@ -1,5 +1,6 @@
 ﻿using CarCatalog.Data.Models;
 using CarCatalog.Web.Data;
+using static CarCatalog.Common.EntityValidationConstants.CarValidations;
 
 namespace CarCatalog.Services.Tests
 {
@@ -9,6 +10,8 @@ namespace CarCatalog.Services.Tests
         public static ApplicationUser CarBuyerUser;
         public static CarSeller Seller;
         public static CarBuyer Buyer;
+        public static CarInfo CarInfo;
+        public static Car Car;
         public static void SeedDatabase(CarCatalogDbContext dbContext)
         {
             CarSellerUser = new ApplicationUser()
@@ -53,11 +56,40 @@ namespace CarCatalog.Services.Tests
                 UserId = CarBuyerUser.Id
             };
 
+            CarInfo = new CarInfo()
+            {
+                Brand = "Citroen",
+                Model = "Xsara Picasso",
+                CarType = "Minivan",
+                HorsePower = 90,
+                EngineDisplacement = 2.0f,
+                Mileage = 150000,
+                Weight = 1300,
+                FuelConsumption = 5.5f,
+                PriceForSale = 7000,
+                Transmission = TransmissionType.Manual,
+                Engine = EngineType.Diesel,
+                ImageUrl = "https://cdn3.focus.bg/autodata/i/citroen/xsara/xsara-picasso-n68/large/93f808a9cfb5a399babc04e50f54eb36.jpg",
+                Description = "A really good family car with low fuel consumption."
+            };
+
+            Car = new Car()
+            {
+                CarInfoId = CarInfo.Id,
+                BuyerId = null,
+                SellerId = Seller.Id,
+                CarDealerId = null
+            };
+
             dbContext.Users.Add(CarSellerUser);
             dbContext.Users.Add(CarBuyerUser);
 
             dbContext.CarSellers.Add(Seller);
             dbContext.CarBuyers.Add(Buyer);
+
+            dbContext.CarInfos.Add(CarInfo);
+            Seller.CarsAvailable.Add(Car);
+            dbContext.Cars.Add(Car);
 
             dbContext.SaveChanges();
         }
